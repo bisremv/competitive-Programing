8ba24fe8738abd731b39ab241f8f9a5387d48f54
bisremv <121819728+bisremv@users.noreply.github.com>
2023-12-31 22:10:15 +0300

    Time: 437 ms (91.18%), Space: 32.8 MB (30.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        l=set()
+        for i in nums:
+            if i in l:
+                return True
+            l.add(i)
+        return False
+        
