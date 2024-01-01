5859595937a535c575f5c77c9b3d57ea4601dc84
bisremv <121819728+bisremv@users.noreply.github.com>
2024-01-01 06:26:11 +0300

    Time: 49 ms (69.43%), Space: 17.5 MB (8.59%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,70 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         ans=0
-        ans2=0
-        out=0
-        temp=""
+        val={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
         for i in range(len(s)):
-            ans=0
-            ans2=0
-            # s[i]=s[i].lower()
-            if len(temp) > 0:
-                temp+=s[i]
-                match temp.lower():
-                    case "cm":
-                        ans+=900
-                    case "cd":
-                        ans+=400
-                    case "xc":
-                        ans+=90
-                    case "xl":
-                        ans+=40
-                    case "ix":
-                        ans+=9
-                    case "iv":
-                        ans+=4
-                    case "i":
-                        ans+=1
+            if i < len(s)-1 and val[s[i]] < val[s[i+1]]:
+                ans-=val[s[i]]
             else:
-                match s[i].lower():
-                    case "m":
-                        ans+=1000
-                    case "d":
-                        ans+=500
-                    case "c":
-                        ans+=100
-                    case "l":
-                        ans+=50
-                    case "x":
-                        ans+=10
-                    case "v":
-                        ans+=5
-                    case "i":
-                        ans+=1
-            if i < len(s)-1 and len(temp)==0:
-                match s[i+1].lower():
-                    case "m":
-                        ans2+=1000
-                    case "d":
-                        ans2+=500
-                    case "c":
-                        ans2+=100
-                    case "l":
-                        ans2+=50
-                    case "x":
-                        ans2+=10
-                    case "v":
-                        ans2+=5
-                    case "i":
-                        ans2+=1
-                if ans >= ans2:
-                    out+=ans
-                else:
-                    temp=s[i]
-            else:
-                temp=""
-                out+=ans
-        return out
-            
-                
-                
+                ans+=val[s[i]]
+        return ans
