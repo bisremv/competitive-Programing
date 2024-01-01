6d8195889b0b2f494c995df550b996a1ce1f9ddb
bisremv <121819728+bisremv@users.noreply.github.com>
2024-01-01 07:16:00 +0300

    Time: 75 ms (5.05%), Space: 17.4 MB (6.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,20 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        for i, char_group in enumerate(zip(*strs)):
-        # Check if all characters in the current group are the same
-            if len(set(char_group)) == 1:
-                continue
-            else:
-            # Found a mismatch, return the common prefix
-                return strs[0][:i]
-
-    # If all characters match up to the length of the shortest string, return that string
-        return min(strs)
-                    
-            
-            
-            
-            
-                
-        
+        ans=""
+        strs=sorted(strs)
+        first=strs[0]
+        last=strs[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
         
