f94b324f4b88d4247f1a1e9785d849e67a6f433a
bisremv <121819728+bisremv@users.noreply.github.com>
2024-01-05 19:32:29 +0300

    Time: 54 ms (78.61%), Space: 17.6 MB (10.48%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -7,23 +7,17 @@ class Solution:
             if numRows ==1:
                 return s
             if count >= numRows:
-                print("Rev++++++")
                 forward=False
             if count == 1 and forward==False:
-                print("Frward++++++")
                 forward=True
             if forward:
                 count+=1
             else:
                 count-=1
             if dict.get(count) !=None:
-                print("add on", count)
                 dict[count].append(i)
-                print(dict[count])
             else:
-                print("new dict",count)
                 dict[count]=[i]
-                print(dict[count])
         ans=[]
         for i in dict.values():
             ans.extend(i)
