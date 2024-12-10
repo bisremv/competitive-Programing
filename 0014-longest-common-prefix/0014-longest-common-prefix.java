f578c330333c8a25d14c57e16e668f20dcd2fa40
Bisrat Yared Girma <121819728+bisremv@users.noreply.github.com>
2024-12-10 06:43:07 +0300

    Time: 0 ms (100%), Space: 41.3 MB (81.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,8 +5,13 @@ class Solution {
         String last=strs[strs.length - 1];
         int minLength = Math.min(first.length(), last.length());
         int i=0;
-        while(i < minLength && first.charAt(i) == last.charAt(i)){
+        while(i < minLength){
+            if(first.charAt(i) == last.charAt(i)){
             i++;
+            }
+            else{
+                break;
+            }
         }
         return first.substring(0, i);
     }
