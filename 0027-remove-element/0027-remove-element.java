d0402a150827a7a227d637cc99db089b03d492ff
Bisrat Yared Girma <121819728+bisremv@users.noreply.github.com>
2024-12-14 23:34:43 +0300

    Time: 0 ms (100%), Space: 42 MB (42.15%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-          int k = 0; // Tracks the count of elements not equal to val
-        
-        // Iterate over the array
-        for (int i = 0; i < nums.length; i++) {
-            // If the current element is not equal to val
-            if (nums[i] != val) {
-                nums[k] = nums[i]; // Move the element to the position k
-                k++; // Increment k
-            }
+      int k = 0;
+      int i=0;
+      for (int n : nums){
+        if (n != val){
+        k++;
+        nums[i]=n;
+        i++;
         }
-        
-        return k; // k is the count of elements not equal to val
+      }  
+      return k;    
     }
 }
